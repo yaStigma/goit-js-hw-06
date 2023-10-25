2e8e9a25690636d12bc8bafd60b491c8e8a5560d
Tanya Yenakieva <yastigma@gmail.com>
2023-10-25 23:40:01 +0300

    task 5 completed

---
+++ js/task-05.js
@@ -0,0 +1,6 @@
+const textInput = document.querySelector("#name-input");
+const spanOutput = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+  spanOutput.textContent = event.currentTarget.value;
+});
