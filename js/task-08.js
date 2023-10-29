4e3c858f0a1d878e7fa27a32af4b6b73c6575281
Tanya Yenakieva <yastigma@gmail.com>
2023-10-29 22:41:49 +0200

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+    
+   if (email.value === "" || password.value === "") {
+        alert ("Всі поля повинні бути заповнені!");
+       return;
+   }
+    
+     const formData = {
+    email: email.value,
+    password: password.value,
+  };
+    console.log(formData);
+    // console.log(`Email: ${email.value},   Password: ${password.value}`);
+    form.reset()
+    
+
+    
+});
+ 
 
