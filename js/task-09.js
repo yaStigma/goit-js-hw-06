ea84042422805cc15a887ded77387139cc816847
Tanya Yenakieva <yastigma@gmail.com>
2023-10-29 23:02:17 +0200

    task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const body = document.body;
+const Btn = document.querySelector(".change-color");
+const text = document.querySelector(".color");
+
+Btn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+  text.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
